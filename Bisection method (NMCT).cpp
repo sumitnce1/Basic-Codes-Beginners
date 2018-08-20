7be7001d99ac820f622b661be40ae01f5dbe94f5
SUMIT <40803138+sumitsingh1111@users.noreply.github.com>
2018-08-20 23:59:54 +0530

    bisection metod
    write the program bisection metod equation solved

---
+++ Bisection method (NMCT).cpp
@@ -0,0 +1,49 @@
+//write the program bisection metod equation solved
+//SUMIT KUMAR
+#include<stdio.h>
+#include<math.h>
+#define ERR 0.01
+double func(double x)
+{
+	//Equation is 2-x^2.
+	//put the Equation 
+	return(2-x*x);
+	
+ }
+ void bisection(double a,double b)
+ {
+ 	int i=0;
+ 	if(func(a)*func(b)>=0.0)
+ 	{
+      	printf("VALUSE ARE OF WORNG A AND B : \n");
+     	return;
+ }
+      double c=a;
+        while((b-a)>=ERR)
+ {
+       	i++;
+      	c=(a+b)/2;
+       	if(func(c)==0.0)
+  	{
+ 	 	break;
+ 	}
+      	else if(func(a)*func(c)<0.0)
+ 	{
+ 		b=c;
+ 	}
+      	else
+      	a=c;
+      	printf("NUMBER OF ITERATION: %d VALUE OF C %f\n",i,c);
+ 	
+ }
+      printf("ROOT :- %lf \n",c);
+      printf("NUMBER OF ITERATION : %d",i);
+      return;
+}
+int main()
+{
+	// a and b is question put the value.
+	 double a=0, b=2;
+	 bisection(a,b);
+	 return 0;
+}
