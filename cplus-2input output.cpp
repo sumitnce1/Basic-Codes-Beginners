b7406fef13058bb9e6326e563fdd969751820b7b
SUMIT <40803138+sumitsingh1111@users.noreply.github.com>
2018-08-21 22:15:33 -0700

    EVEN OR ODD.
    WRITE A C++ PROGRAM CHEACK WHETHER NUMBER IS EVEN OR ODD.

---
+++ cplus-2input output.cpp
@@ -0,0 +1,15 @@
+//WRITE THE PROGRAM IN C++ INPUT\OUTPUT
+//SUMIT KUMAR
+#include<iostream>
+using namespace std;
+int main()
+{
+    int Test, s,u;
+    cin>>Test;
+    for(int i=0;i<Test;i++)
+    {
+        cin>>s>>u;
+        cout<<s*u<<endl;
+    }
+      return 0;
+}
