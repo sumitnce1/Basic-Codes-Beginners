d5373c1937685367bdce004302adec3fa9267e20
SUMIT <40803138+sumitsingh1111@users.noreply.github.com>
2018-08-23 10:26:33 -0700

    Even or Odd (Ternary Operators).
    *Write a c++ program Even or Odd (Ternary Operators).

---
+++ Even or Odd (Ternary Operators).cpp
@@ -0,0 +1,16 @@
+/* Write a c++ program Even or Odd (Ternary Operators).
+ SUMIT KUMAR
+ */
+ 
+#include<iostream>
+using namespace std;
+int main()
+{
+	int n;
+		{
+			cout<<"ENTER A NUMBER:- ";
+			cin>>n;
+		    (n%2==0)?cout<<n<<":-IS EVEN NUMBER.":cout<<n<<":-IS ODD NUMBER.";
+			return 0;
+		}
+}
