334aabc52a1e1f2f8d7f93e7dc5e07a3eaff8838
SUMIT <40803138+sumitsingh1111@users.noreply.github.com>
2018-08-23 14:27:22 +0530

    Replace number{two files-sumit.h & sumitheader.cpp
    WRITE A PROGRAM TO REPLACE 0 IN NUMBER WITH 5 IN C++.
    * Replace number{two files-sumit.h & sumitheader.cpp

---
+++ conver header/sumitheader.cpp
@@ -0,0 +1,14 @@
+#include"sumit.h"
+#include<iostream>
+using namespace std;
+int main()
+{
+ int num;
+   cout<<" ENTER ANY NUMBER:- ";
+   cin>>num;
+ int convertednum=convert(num);
+   cout<<convertednum <<endl;
+
+  return 0;
+
+} 
